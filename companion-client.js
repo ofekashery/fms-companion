cf9f9572542cdd1dcb220b1b82732c7747ba8730
Ofek Ashery <ofek.ashery@gmail.com>
2022-03-21 01:19:31 +0200

    Fix protocol

--- companion-client.js
+++ companion-client.js
@@ -2,10 +2,6 @@ const WebSocket = require("ws");
 const fetch = require("node-fetch");
 
 const mapButtons = {
-  "Field 1": {
-    page: 1,
-    button: 2,
-  },
   FMS_PREVIEW: {
     page: 1,
     button: 0,
@@ -38,25 +34,48 @@ const mapButtons = {
 
 const fmsServer = new WebSocket("ws://64.227.44.99:3323");
 
+const camelToScore = (key) => {
+  const result = key.replace(/([A-Z])/g, " $1");
+  return result.split(" ").join("-").toLowerCase();
+};
+
 fmsServer.on("open", () => {
   console.log("FMS Server connected");
 });
 
 fmsServer.on("message", (data) => {
   const json = JSON.parse(data.toString());
-  const reply = (msg) =>
+  const reply = (msg) => {
+    const data = {
+      messageId: json["message-id"],
+      status: "ok",
+      ...msg,
+    };
+
     fmsServer.send(
       JSON.stringify({
-        "message-id": json["message-id"],
-        status: "ok",
-        ...msg,
+        ...data,
+        ...Object.fromEntries(
+          Object.entries(data).map(([key, val]) => [camelToScore(key), val])
+        ),
       })
     );
+  };
 
   const requestType = json["request-type"];
 
   if (requestType === "GetAuthRequired") {
     return reply({ authRequired: false });
+  } else if (requestType === "GetVersion") {
+    return reply({
+      version: 1.1,
+      availableRequests:
+        "AddFilterToSource,AddSceneItem,Authenticate,BroadcastCustomMessage,CreateScene,CreateSource,DeleteSceneItem,DisableStudioMode,DuplicateSceneItem,EnableStudioMode,ExecuteBatch,GetAudioActive,GetAudioMonitorType,GetAudioTracks,GetAuthRequired,GetBrowserSourceProperties,GetCurrentProfile,GetCurrentScene,GetCurrentSceneCollection,GetCurrentTransition,GetFilenameFormatting,GetMediaDuration,GetMediaSourcesList,GetMediaState,GetMediaTime,GetMute,GetOutputInfo,GetPreviewScene,GetRecordingFolder,GetRecordingStatus,GetReplayBufferStatus,GetSceneItemList,GetSceneItemProperties,GetSceneList,GetSceneTransitionOverride,GetSourceActive,GetSourceDefaultSettings,GetSourceFilterInfo,GetSourceFilters,GetSourceSettings,GetSourcesList,GetSourceTypesList,GetSpecialSources,GetStats,GetStreamingStatus,GetStreamSettings,GetStudioModeStatus,GetSyncOffset,GetTextFreetype2Properties,GetTextGDIPlusProperties,GetTransitionDuration,GetTransitionList,GetTransitionPosition,GetTransitionSettings,GetVersion,GetVideoInfo,GetVirtualCamStatus,GetVolume,ListOutputs,ListProfiles,ListSceneCollections,MoveSourceFilter,NextMedia,OpenProjector,PauseRecording,PlayPauseMedia,PreviousMedia,RefreshBrowserSource,ReleaseTBar,RemoveFilterFromSource,RemoveSceneTransitionOverride,ReorderSceneItems,ReorderSourceFilter,ResetSceneItem,RestartMedia,ResumeRecording,SaveReplayBuffer,SaveStreamSettings,ScrubMedia,SendCaptions,SetAudioMonitorType,SetAudioTracks,SetBrowserSourceProperties,SetCurrentProfile,SetCurrentScene,SetCurrentSceneCollection,SetCurrentTransition,SetFilenameFormatting,SetHeartbeat,SetMediaTime,SetMute,SetPreviewScene,SetRecordingFolder,SetSceneItemCrop,SetSceneItemPosition,SetSceneItemProperties,SetSceneItemRender,SetSceneItemTransform,SetSceneTransitionOverride,SetSourceFilterSettings,SetSourceFilterVisibility,SetSourceName,SetSourceRender,SetSourceSettings,SetStreamSettings,SetSyncOffset,SetTBarPosition,SetTextFreetype2Properties,SetTextGDIPlusProperties,SetTransitionDuration,SetTransitionSettings,SetVolume,Sleep,StartOutput,StartRecording,StartReplayBuffer,StartStopRecording,StartStopReplayBuffer,StartStopStreaming,StartStopVirtualCam,StartStreaming,StartVirtualCam,StopMedia,StopOutput,StopRecording,StopReplayBuffer,StopStreaming,StopVirtualCam,TakeSourceScreenshot,ToggleMute,ToggleStudioMode,TransitionToProgram,TriggerHotkeyByName,TriggerHotkeyBySequence",
+      obsStudioVersion: "27.2.2",
+      obsWebsocketVersion: "4.9.1",
+      supportedImageExportFormats:
+        "bmp,cur,heic,heif,icns,ico,jp2,jpeg,jpg,pbm,pgm,png,ppm,tif,tiff,wbmp,webp,xbm,xpm",
+    });
   } else if (requestType === "GetSceneList") {
     return reply({
       currentScene: "FIRST",
